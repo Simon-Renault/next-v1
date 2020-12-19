a46ef32349a78e7433188af4cdac680b5011a2e2
simon <simon.renault.pro@gmail.com>
2020-12-19 15:59:31 +0000

    burger

--- components/header.tsx
+++ components/header.tsx
@@ -1,26 +1,31 @@
 
 import HeaderLink from '@/components/header-link'
 import Link from 'next/link'
-import { Menu } from 'react-feather'
+import MenuBurger from '@/components/MenuBurger'
+import { motion,useCycle } from 'framer-motion';
 
-const Header = () : JSX.Element => {
+
+const Header = (): JSX.Element => {
+	const [isOpen, toggleOpen] = useCycle(false, true);
+
 	return (
 		<header className="fixed top-0 left-0 right-0 z-50 flex flex-row h-20 p-5 bg-white border-b border-gray-100">
 
 			<div className="flex flex-row items-center justify-between w-full m-auto transition-all md:max-w-6xl">
 
 				<Link href="/">
-					<a className="flex flex-row items-center justify-center ">
+					<a className="flex flex-row items-center justify-center">
 						<div className="w-10 h-10 mr-3 bg-center bg-contain rounded-full bg-logo"></div>
 						<div className="font-sans text-sm font-medium uppercase">Simon Renault</div>
 					</a>
 				</Link>
-			
-				<div className="p-3 cursor-pointer hover:bg-gray-50 md:hidden">
-					<Menu size={20}/>
-				</div>
-				
-				<nav className="fixed left-0 right-0 flex flex-col p-5 bg-white border-b border-gray-100 top-20 md:static md:border-b-0 md:flex-row md:p-0">
+
+				<motion.div  initial={false} animate={isOpen ? "open" : "closed"} className="flex h-full p-2 rounded cursor-pointer hover:bg-gray-50 md:hidden">
+
+					<MenuBurger toggle={() => toggleOpen()}/>
+				</motion.div>
+
+				<nav className="fixed left-0 right-0 flex-col hidden p-5 bg-white border-b border-gray-100 md:flex top-20 md:static md:border-b-0 md:flex-row md:p-0">
 					<HeaderLink href="/">Home</HeaderLink>
 					<HeaderLink href="/collection">Collection</HeaderLink>
 					<HeaderLink href="/drawing">Drawing</HeaderLink>
@@ -31,5 +36,6 @@ const Header = () : JSX.Element => {
 		</header>
 	)
 }
+
 
 export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,18 @@
 import { motion } from 'framer-motion';
 import Head from 'next/head';
 
-export default function Home(): JSX.Element {
-	return (
-		<>
-			<Head>
-				<title>Home</title>
-				<meta name="description" content=""></meta>
-			</Head>
-			<motion.div initial="initial" animate="animate" exit="exit" key="Home" className="p-5 pt-20 Home" >
-				<div className="md:max-w-6xl" >
-				
-				</div>
-			</motion.div>
-		</>
-	);
-}
+const Home = (): JSX.Element => (
+	<>
+		<Head>
+			<title>Home</title>
+			<meta name="description" content=""></meta>
+		</Head>
+		<motion.div initial="initial" animate="animate" exit="exit" key="Home" className="p-5 pt-20 Home" >
+			<div className="md:max-w-6xl" >
+			
+			</div>
+		</motion.div>
+	</>
+);
+
+export default Home
