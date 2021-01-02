0a44196498421bf52e0195d883366e5f07eae81c
simon <simon.renault.pro@gmail.com>
2021-01-02 17:52:47 +0000

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,81 @@ const Home = (): JSX.Element => {
 					className={css.hero_image}
 					src="https://simonr.s3.eu-west-2.amazonaws.com/elephant.jpeg"
 				/>
+
+				<main id="main" className="main">
+					<div className={css.text_center}>
+						<h2>A large title</h2>
+						<p>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet harum voluptatibus dolorem
+							corporis repellendus, dignissimos sunt aliquid quo! Veritatis aspernatur quisquam doloribus
+							laudantium dolorem tempore, nobis dolores, dolor numquam corporis similique perspiciatis.
+							Perferendis nostr
+						</p>
+						<button>Gallerie</button>
+					</div>
+
+					<div className={css.gallery_preview}>
+						<div className={css.col}>
+							<Img
+								alt="Cover image showing a detailed elephant drawing"
+								style={{
+									paddingBottom: '160%',
+								}}
+								src="https://simonr.s3.eu-west-2.amazonaws.com/tower2.jpg"
+							/>
+							<Img
+								alt="Cover image showing a detailed elephant drawing"
+								style={{
+									paddingBottom: '100%',
+								}}
+								src="https://simonr.s3.eu-west-2.amazonaws.com/beast.jpg"
+							/>
+						</div>
+						<div className={css.col}>
+							<Img
+								alt="Cover image showing a detailed elephant drawing"
+								style={{
+									paddingBottom: '120%',
+								}}
+								src="https://simonr.s3.eu-west-2.amazonaws.com/tree.jpg"
+							/>
+							<Img
+								alt="Cover image showing a detailed elephant drawing"
+								style={{
+									paddingBottom: '170%',
+								}}
+								src="https://simonr.s3.eu-west-2.amazonaws.com/flying.jpg"
+							/>
+						</div>
+						<div className={css.col}>
+							<Img
+								alt="Cover image showing a detailed elephant drawing"
+								style={{
+									paddingBottom: '160%',
+								}}
+								src="https://simonr.s3.eu-west-2.amazonaws.com/farm_1.jpg"
+							/>
+							<Img
+								alt="Cover image showing a detailed elephant drawing"
+								style={{
+									paddingBottom: '100%',
+								}}
+								src="https://simonr.s3.eu-west-2.amazonaws.com/top.jpeg"
+							/>
+						</div>
+					</div>
+					<div className="collections"></div>
+					<div className={css.text_center}>
+						<h2>A large title</h2>
+						<p>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet harum voluptatibus dolorem
+							corporis repellendus, dignissimos sunt aliquid quo! Veritatis aspernatur quisquam doloribus
+							laudantium dolorem tempore, nobis dolores, dolor numquam corporis similique perspiciatis.
+							Perferendis nostr
+						</p>
+					</div>
+					<div className="prints"></div>
+				</main>
 			</motion.div>
 		</>
 	);
